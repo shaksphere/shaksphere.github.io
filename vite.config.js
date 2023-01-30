9d12dbd8bfdb6778b5188a64c0708959f65cd140
shaksphere <110363828+shaksphere@users.noreply.github.com>
2023-01-30 22:20:29 +1100

    fix asset links #1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shaksphere.github.io/',
   plugins: [react()],
+  base: '/shaksphere.github.io/',
+
 })
